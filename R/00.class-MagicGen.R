63c20e8e8ec6cd36b80a8fcbc75b82ec6072cc32
Hugo Tavares <hugo@slpc253.slcu.private.cam.ac.uk>
2016-09-27 17:16:30 +0100

    Add new class MagicGenPhen

--- R/00.class-MagicGen.R
+++ R/00.class-MagicGen.R
@@ -1,12 +1,27 @@
 #' MagicGen class
 #'
-#' @slot phenotypes data.frame with phenotypes for each MAGIC line. 
 #' @slot markers data.frame with marker names and positions. 
 #' @slot genotypes list with genotype probability matrices. 
 #'
 #' @rdname class-MagicGen
 #' @aliases MagicGen
 setClass("MagicGen",
-				 representation(phenotypes = "data.frame",
-				 							 markers = "data.frame", 
+				 representation(markers = "data.frame", 
 				 							 genotypes = "list"))
+
+
+#' MagicGenPhen class
+#'
+#' @slot phenotypes data.frame. 
+#' @slot markers data.frame. 
+#' @slot genotypes list. 
+#'
+#' @rdname class-MagicGen
+#' @aliases MagicGenPhen
+setClass("MagicGenPhen",
+         representation(phenotypes = "data.frame",
+                        markers = "data.frame", 
+                        genotypes = "list"),
+         contains = "MagicGen")
+
+
